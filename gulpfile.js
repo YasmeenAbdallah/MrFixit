ca3d80053fad174b976d463946ef55e68d671457
yassmeena <zulema11898@gmail.com>
2020-03-09 16:00:30 +0200

    finish the gulp file and create the sass files

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// define gulp
+const gulp = require('gulp');
+
+// define sass 
+const sass = require('gulp-sass');
+
+// define htmlmin
+const htmlmin = require('gulp-htmlmin');
+
+//compile sass 
+sass.compiler = require('node-sass');
+
+// minify css 
+cssmin = require('gulp-cssnano');
+// sass function
+gulp.task('sass', () => {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(cssmin())
+        .pipe(gulp.dest('./des'));
+});
+
+// html function
+gulp.task('htmlmin', () => {
+    return gulp.src('./*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./des'));
+});
+
+// sass watch file 
+gulp.task('watch', () => {
+    gulp.watch('./scss/**/*.*', gulp.series('sass'));
+});
+//work the tasks sequence
+gulp.task('default', gulp.series('sass', 'htmlmin','watch'));
